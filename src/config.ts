f2cb414a1718190b54f66aca1cc5a64172ac6a73
German Robayo <germanrobayo33@gmail.com>
2019-10-01 19:34:32 -0400

    add knex config

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,13 @@
 import { config } from 'dotenv'
 config()
+import knex from 'knex'
 
+const db = knex({
+  client: 'pg',
+  connection: process.env.PG_CONNECTION_STRING,
+})
 
 export default {
-  PORT: process.env.PORT
+  PORT: process.env.PORT,
+  db: db
 }
