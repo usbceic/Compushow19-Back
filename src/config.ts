f7128db353933b7eb00ee4cd43f5f7a46dc01775
German Robayo <germanrobayo33@gmail.com>
2019-10-10 12:00:09 -0400

    refactor configs

--- src/config.ts
+++ src/config.ts
@@ -5,10 +5,12 @@ config({
   path: path.resolve(process.cwd(), process.env.ENV_FILE || '.env')
 })
 
-export const db = knex({
-  client: 'pg',
-  connection: process.env.PG_CONNECTION_STRING,
-})
-
 export const PORT : string = process.env.PORT!
 export const NODE_ENV : string = process.env.NODE_ENV!
+export const PG_CONNECTION_STRING : string = process.env.PG_CONNECTION_STRING!
+
+export const db = knex({
+  client: 'pg',
+  connection: PG_CONNECTION_STRING,
+})
+
