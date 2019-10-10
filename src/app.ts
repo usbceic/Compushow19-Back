ef6fb91fdda3a02227b76060460cd3ae394e36c5
German Robayo <germanrobayo33@gmail.com>
2019-10-10 11:46:42 -0400

    only log when NODE_ENV is not test

--- src/app.ts
+++ src/app.ts
@@ -2,26 +2,32 @@ import express from 'express'
 import expressWinston from 'express-winston'
 import winston from 'winston'
 import errorHandler from './errorHandling/errorHandler'
+import userRoutes from './users/routes'
+import { NODE_ENV } from './config'
 
 const app = express()
 
-app.use(expressWinston.logger({
-  transports: [
-    new winston.transports.Console()
-  ],
-  format: winston.format.combine(
-    winston.format.colorize(),
-    winston.format.simple()
-  ),
-  meta: false,
-  msg: 'HTTP {{req.method}} {{req.url}}',
-  colorize: true
-}))
+if (NODE_ENV !== 'ci' && NODE_ENV !== 'test') {
+  app.use(expressWinston.logger({
+    transports: [
+      new winston.transports.Console()
+    ],
+    format: winston.format.combine(
+      winston.format.colorize(),
+      winston.format.simple()
+    ),
+    meta: false,
+    msg: 'HTTP {{req.method}} {{req.url}}',
+    colorize: true
+  }))
+}
 
 
 app.use('/health', (req, res) => {
   res.json({ status: 'UP' })
 })
+
+app.use('/v1/api', userRoutes)
 
 app.use(errorHandler)
 
