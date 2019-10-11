d8ff215628f1d3e781018545de32cf3578fef516
German Robayo <germanrobayo33@gmail.com>
2019-10-10 22:04:20 -0400

    add tests for email

--- tests/user-endpoints/user.test.ts
+++ tests/user-endpoints/user.test.ts
@@ -32,8 +32,7 @@ describe('User management', () => {
       expect(user.profileUrl).toBe(expected.profileUrl)
       expect(user.studentId).toBe(expected.studentId)
     })
-    it('Returns 404 when `fullName` is invalid', async () => {
-      // fullName should be present
+    it('Returns 400 when `fullName` is invalid', async () => {
       const requiredResponse = await request(app)
         .post(url)
         .send({
@@ -42,11 +41,11 @@ describe('User management', () => {
           profileUrl: 'https://photo.url',
           studentId: '11-11111'
         })
+      expect(requiredResponse.status).toBe(400)
       expect(requiredResponse.body.errors).toContainEqual({
         field: 'fullName',
         validationCode: 'fullName.REQUIRED'
       })
-      // fullName should be present
       const shouldBeStringResponse = await request(app)
         .post(url)
         .send({
@@ -56,11 +55,11 @@ describe('User management', () => {
           studentId: '11-11111',
           fullName: 1
         })
+      expect(shouldBeStringResponse.status).toBe(400)
       expect(shouldBeStringResponse.body.errors).toContainEqual({
         field: 'fullName',
         validationCode: 'fullName.STRING'
       })
-      // fullName should be present
       const stringNotEmptyResponse = await request(app)
         .post(url)
         .send({
@@ -70,11 +69,11 @@ describe('User management', () => {
           studentId: '11-11111',
           fullName: ''
         })
+      expect(stringNotEmptyResponse.status).toBe(400)
       expect(stringNotEmptyResponse.body.errors).toContainEqual({
         field: 'fullName',
         validationCode: 'fullName.LENGTH_NOT_VALID'
       })
-      // fullName should be present
       const maxStringResponse = await request(app)
         .post(url)
         .send({
@@ -82,11 +81,71 @@ describe('User management', () => {
           canVote: true,
           profileUrl: 'https://photo.url',
           studentId: '11-11111',
-          fullName: ''
+          fullName: '111111111111111111111111111111111111111111111111111'
         })
+      expect(maxStringResponse.status).toBe(400)
       expect(maxStringResponse.body.errors).toContainEqual({
         field: 'fullName',
         validationCode: 'fullName.LENGTH_NOT_VALID'
+      })
+
+    })
+
+    it('Returns 400 when `email` is invalid', async () => {
+      const baseRequest = {
+        fullName: 'Test Name',
+        canVote: true,
+        profileUrl: 'https://photo.url',
+        studentId: '11-11111'
+      }
+      const requiredResponse = await request(app)
+        .post(url)
+        .send({
+          ...baseRequest
+        })
+      expect(requiredResponse.body.errors).toContainEqual({
+        field: 'email',
+        validationCode: 'email.REQUIRED'
+      })
+      const shouldBeStringResponse = await request(app)
+        .post(url)
+        .send({
+          ...baseRequest,
+          email: 1
+        })
+      expect(shouldBeStringResponse.body.errors).toContainEqual({
+        field: 'email',
+        validationCode: 'email.STRING'
+      })
+      const shouldBeEmailResponse = await request(app)
+        .post(url)
+        .send({
+          ...baseRequest,
+          email: 'not-an-email'
+        })
+      expect(shouldBeEmailResponse.body.errors).toContainEqual({
+        field: 'email',
+        validationCode: 'email.EMAIL'
+      })
+      const stringNotEmptyResponse = await request(app)
+        .post(url)
+        .send({
+          ...baseRequest,
+          email: ''
+        })
+      expect(stringNotEmptyResponse.body.errors).toContainEqual({
+        field: 'email',
+        validationCode: 'email.LENGTH_NOT_VALID'
+      })
+      const maxStringResponse = await request(app)
+        .post(url)
+        .send({
+          ...baseRequest,
+          email: 'very-very-very-very-long-long-long@email.email.comm'
+        })
+      expect(maxStringResponse.body.errors).toContainEqual({
+        field: 'email',
+        validationCode: 'email.LENGTH_NOT_VALID'
       })
 
     })
