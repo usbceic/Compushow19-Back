520dd03a7622a99d4555968175b70950932fc320
German Robayo <germanrobayo33@gmail.com>
2019-10-10 20:49:05 -0400

    nest tests

--- tests/user-endpoints/user.test.ts
+++ tests/user-endpoints/user.test.ts
@@ -11,23 +11,25 @@ beforeAll(async () => {
 
 describe('User management', () => {
   const baseUrl = '/v1/api'
-  it('Allows user creation', async () => {
-    const expected : CreateUserRequest = {
-      fullName: 'Test User',
-      email: 'test@test.com',
-      canVote: true,
-      profileUrl: 'https://photo.url',
-      studentId: '11-11111'
-    }
-    const res = await request(app)
-      .post(`${baseUrl}/users`)
-      .send(expected)
-    expect(res.status).toBe(204)
-    const user = await getUserByEmailAddress('test@test.com')
-    expect(user.fullName).toBe(expected.fullName)
-    expect(user.email).toBe(expected.email)
-    expect(user.canVote).toBe(expected.canVote)
-    expect(user.profileUrl).toBe(expected.profileUrl)
-    expect(user.studentId).toBe(expected.studentId)
+  describe('User creation', () => {
+    it('Allows user creation', async () => {
+      const expected : CreateUserRequest = {
+        fullName: 'Test User',
+        email: 'test@test.com',
+        canVote: true,
+        profileUrl: 'https://photo.url',
+        studentId: '11-11111'
+      }
+      const res = await request(app)
+        .post(`${baseUrl}/users`)
+        .send(expected)
+      expect(res.status).toBe(204)
+      const user = await getUserByEmailAddress('test@test.com')
+      expect(user.fullName).toBe(expected.fullName)
+      expect(user.email).toBe(expected.email)
+      expect(user.canVote).toBe(expected.canVote)
+      expect(user.profileUrl).toBe(expected.profileUrl)
+      expect(user.studentId).toBe(expected.studentId)
+    })
   })
 })
