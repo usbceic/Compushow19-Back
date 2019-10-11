e5a31e8efea8dd1f6095dbe188b9b5e2ec8bf5ea
German Robayo <germanrobayo33@gmail.com>
2019-10-10 22:21:13 -0400

    refactor tests

--- tests/user-endpoints/user.test.ts
+++ tests/user-endpoints/user.test.ts
@@ -32,141 +32,104 @@ describe('User management', () => {
       expect(user.profileUrl).toBe(expected.profileUrl)
       expect(user.studentId).toBe(expected.studentId)
     })
-    describe('Returns 400 when `fullName` is invalid', () => {
-      it('verifies that the field is on the request', async () => {
+    describe('Data validation', () => {
+      async function runTest(payload: any, expectedError: any) {
         const response = await request(app)
           .post(url)
-          .send({
-            email: 'test@test.com',
-            canVote: true,
-            profileUrl: 'https://photo.url',
-            studentId: '11-11111'
-          })
+          .send(payload)
         expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'fullName',
-          validationCode: 'fullName.REQUIRED'
-        })
-      })
-      it('verifies that the field is an string', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
-            email: 'test@test.com',
-            canVote: true,
-            profileUrl: 'https://photo.url',
-            studentId: '11-11111',
-            fullName: 1
-          })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'fullName',
-          validationCode: 'fullName.STRING'
-        })
-      })
-      it('verifies that the string is not empty', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
-            email: 'test@test.com',
-            canVote: true,
-            profileUrl: 'https://photo.url',
-            studentId: '11-11111',
-            fullName: ''
-          })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'fullName',
-          validationCode: 'fullName.LENGTH_NOT_VALID'
-        })
-      })
-      it('verifieds that the string doesn\'t exceeds 50 chars', async () => {
-        const maxStringResponse = await request(app)
-          .post(url)
-          .send({
-            email: 'test@test.com',
-            canVote: true,
-            profileUrl: 'https://photo.url',
-            studentId: '11-11111',
-            fullName: '111111111111111111111111111111111111111111111111111'
-          })
-        expect(maxStringResponse.status).toBe(400)
-        expect(maxStringResponse.body.errors).toContainEqual({
-          field: 'fullName',
-          validationCode: 'fullName.LENGTH_NOT_VALID'
-        })
-      })
-    })
-
-    describe('Returns 400 when `email` is invalid', () => {
-      const baseRequest = {
-        fullName: 'Test Name',
-        canVote: true,
-        profileUrl: 'https://photo.url',
-        studentId: '11-11111'
+        expect(response.body.errors).toContainEqual(expectedError)
       }
-      it('verifies that the field is in the request', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
-            ...baseRequest
+      describe('Returns 400 when `fullName` is invalid', () => {
+        const baseResponse = {
+          email: 'test@test.com',
+          canVote: true,
+          profileUrl: 'https://photo.url',
+          studentId: '11-11111'
+        }
+        it('verifies that the field is on the request', async () => {
+          await runTest(baseResponse, {
+            field: 'fullName',
+            validationCode: 'fullName.REQUIRED'
           })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'email',
-          validationCode: 'email.REQUIRED'
+        })
+        it('verifies that the field is an string', async () => {
+          await runTest({
+            ...baseResponse,
+            fullName: 1
+          }, {
+            field: 'fullName',
+            validationCode: 'fullName.STRING'
+          })
+        })
+        it('verifies that the string is not empty', async () => {
+          await runTest({
+            ...baseResponse,
+            fullName: ''
+          }, {
+            field: 'fullName',
+            validationCode: 'fullName.LENGTH_NOT_VALID'
+          })
+        })
+        it('verifieds that the string doesn\'t exceeds 50 chars', async () => {
+          await runTest({
+            ...baseResponse,
+            fullName: '111111111111111111111111111111111111111111111111111'
+          }, {
+            field: 'fullName',
+            validationCode: 'fullName.LENGTH_NOT_VALID'
+          })
         })
       })
-      it('verifies that the field is an string', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
+
+      describe('Returns 400 when `email` is invalid', () => {
+        const baseRequest = {
+          fullName: 'Test Name',
+          canVote: true,
+          profileUrl: 'https://photo.url',
+          studentId: '11-11111'
+        }
+        it('verifies that the field is in the request', async () => {
+          await runTest(baseRequest, {
+            field: 'email',
+            validationCode: 'email.REQUIRED'
+          })
+        })
+        it('verifies that the field is an string', async () => {
+          await runTest({
             ...baseRequest,
             email: 1
+          }, {
+            field: 'email',
+            validationCode: 'email.STRING'
           })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'email',
-          validationCode: 'email.STRING'
         })
-      })
-      it('verifies that the field is an email', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
+        it('verifies that the field is an email', async () => {
+          await runTest({
             ...baseRequest,
             email: 'not-an-email'
+          }, {
+            field: 'email',
+            validationCode: 'email.EMAIL'
           })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'email',
-          validationCode: 'email.EMAIL'
         })
-      })
-      it('verifies that the field is an email', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
+        it('verifies that the field is an email', async () => {
+          await runTest({
             ...baseRequest,
             email: ''
+          }, {
+            field: 'email',
+            validationCode: 'email.LENGTH_NOT_VALID'
           })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'email',
-          validationCode: 'email.LENGTH_NOT_VALID'
         })
-      })
-      it('verifies that the field doesn\'t exceed 50 chars', async () => {
-        const response = await request(app)
-          .post(url)
-          .send({
+        it('verifies that the field doesn\'t exceed 50 chars', async () => {
+          await runTest({
             ...baseRequest,
             email: 'very-very-very-very-long-long-long@email.email.comm'
+          }, {
+            field: 'email',
+            validationCode: 'email.LENGTH_NOT_VALID'
           })
-        expect(response.status).toBe(400)
-        expect(response.body.errors).toContainEqual({
-          field: 'email',
-          validationCode: 'email.LENGTH_NOT_VALID'
         })
       })
     })
