2dde9469e4cb72b6d0b8737524d27821cb5536ea
German Robayo <germanrobayo33@gmail.com>
2019-10-11 22:53:00 -0400

    async every test

--- tests/utility-endpoints/health.test.ts
+++ tests/utility-endpoints/health.test.ts
@@ -2,8 +2,8 @@ import request from 'supertest'
 import app from '../../src/app'
 
 describe('/health endpoint', () => {
-  test('returns http status code 200', () => {
-    return request(app)
+  test('returns http status code 200', async () => {
+    await request(app)
       .get('/health')
       .expect(200)
       .expect({
