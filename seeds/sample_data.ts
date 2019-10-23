1965fa2f9a86ace24a22e8772b75cebb774a4694
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-10-22 23:27:27 -0400

    expand seed to delete all tables

--- seeds/sample_data.ts
+++ seeds/sample_data.ts
@@ -2,6 +2,11 @@ import * as Knex from 'knex'
 
 export async function seed(knex: Knex): Promise<any> {
   // Deletes ALL existing entries
-  const usersTable = 'users'
-  await knex(usersTable).del()
+  const TABLE_NAMES = [
+    'users',
+    'categories'
+  ]
+  TABLE_NAMES.forEach(async (name) => {
+    await knex(name).del()
+  })
 }
