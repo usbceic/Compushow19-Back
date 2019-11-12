d27383a18a04459f2101ce4ae32c25b11c90ce2a
German Robayo <germanrobayo33@gmail.com>
2019-11-12 19:16:41 -0400

    add conf for google sign in

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,28 @@
 import { config } from 'dotenv'
 import knex from 'knex'
 import path from 'path'
+
 config({
   path: path.resolve(process.cwd(), process.env.ENV_FILE || '.env')
 })
 
-export const PORT : string = process.env.PORT! || '8080'
-export const NODE_ENV : string = process.env.NODE_ENV!
-export const PG_CONNECTION_STRING : string = process.env.PG_CONNECTION_STRING!
+const req = (envVar: string) : string => {
+  const variable : string | undefined = process.env[envVar]
+  if (variable === undefined) {
+    throw new Error(`Required variable ${envVar} is not defined`)
+  }
+  return variable
+}
+
+const setDefault = (envVar: string, defValue: string) : string => process.env[envVar] || defValue
+
+
+process.env
+export const PORT : string = setDefault('PORT', '8080')
+export const NODE_ENV : string = req('NODE_ENV')
+export const PG_CONNECTION_STRING : string = req('PG_CONNECTION_STRING')
+export const GOOGLE_SIGN_IN_CLIENT_ID : string = req('GOOGLE_SIGN_IN_CLIENT_ID')
+export const GOOGLE_EMAIL_DOMAIN : string = req('GOOGLE_EMAIL_DOMAIN')
 
 export const db = knex({
   client: 'pg',
