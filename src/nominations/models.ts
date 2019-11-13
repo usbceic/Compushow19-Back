4d9c812ffb2b29b753271849fde778b2a542b4b2
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-11-12 20:08:36 -0400

    add query functions for nominations

--- src/nominations/models.ts
+++ src/nominations/models.ts
@@ -19,3 +19,45 @@ export interface NominationModel {
   auxNominee: number
   extra?: string
 }
+
+export async function getNominationById(id: number) : Promise<NominationModel> {
+  return await db(TABLE_NAME).where('id', id).first()
+}
+
+export async function getNominationByUserAndCategory(userId: number, categoryId: number) : Promise<NominationModel> {
+  return await db(TABLE_NAME)
+    .select(NOMINATION_FIELDS)
+    .where('userId', userId)
+    .where('categoryId', categoryId)
+    .first()
+}
+
+export async function getAllNominations() : Promise<[NominationModel]> {
+  return await db(TABLE_NAME)
+    .select(NOMINATION_FIELDS)
+    .then((nominations) => {
+      return <[NominationModel]>nominations
+    })
+}
+
+export async function getNominationsByUserId(userId: number) : Promise<[NominationModel]> {
+  return await db(TABLE_NAME)
+    .select(NOMINATION_FIELDS)
+    .where('userId', userId)
+    .then((nominations) => {
+      return <[NominationModel]>nominations
+    })
+}
+
+export async function nominationExistsById(id: number) : Promise<boolean> {
+  return (await getNominationById(id)) !== undefined
+}
+
+export async function insertNomination(nomination: NominationModel) : Promise<NominationModel> {
+  return await db(TABLE_NAME)
+    .returning(NOMINATION_FIELDS).insert(nomination)
+}
+
+export async function deleteNominationById(id: number) : Promise<boolean> {
+  return await db(TABLE_NAME).where('id', id).del()
+}
