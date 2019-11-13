9f8a5904287d832fe1ea95a189b28d7e9716aeba
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-11-13 02:37:15 -0400

    handle user/category deletion on database

--- migrations/20191112193513_create_nominations_table.ts
+++ migrations/20191112193513_create_nominations_table.ts
@@ -5,10 +5,10 @@ export async function up(knex: Knex): Promise<any> {
   return knex.schema
     .createTable('nominations', table => {
       table.increments('id').unique()
-      table.integer('userId').unsigned().index().references('id').inTable('users').notNullable()
-      table.integer('categoryId').unsigned().index().references('id').inTable('categories').notNullable()
-      table.integer('mainNominee').unsigned().index().references('id').inTable('users')
-      table.integer('auxNominee').unsigned().index().references('id').inTable('users')
+      table.integer('userId').unsigned().index().references('id').inTable('users').notNullable().onDelete('CASCADE')
+      table.integer('categoryId').unsigned().index().references('id').inTable('categories').notNullable().onDelete('CASCADE')
+      table.integer('mainNominee').unsigned().index().references('id').inTable('users').onDelete('SET NULL')
+      table.integer('auxNominee').unsigned().index().references('id').inTable('users').onDelete('SET NULL')
       table.string('extra', 255)
     })
 }
