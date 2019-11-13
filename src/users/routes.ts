628d6f7e1547d62cac5473d1c5687f4d0793797c
German Robayo <germanrobayo33@gmail.com>
2019-11-12 21:13:46 -0400

    update post -> get

--- src/users/routes.ts
+++ src/users/routes.ts
@@ -16,7 +16,7 @@ router.post('', validateRequest(userSchemaValidator), asyncWrap(async (req, res)
   res.status(201).json(user)
 }))
 
-router.post('/:userId([0-9]+)', asyncWrap(async (req, res) => {
+router.get('/:userId([0-9]+)', asyncWrap(async (req, res) => {
   const user = await getUser(parseInt(req.params.userId))
   res.status(200).json(user)
 }))
