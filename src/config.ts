07b0a640fc0f70c29051ed4682543b33e624a62a
German Robayo <germanrobayo33@gmail.com>
2019-11-12 22:09:40 -0400

    add ADMINS variable

--- src/config.ts
+++ src/config.ts
@@ -10,7 +10,7 @@ config({
 
 const req = (envVar: string) : string => {
   const variable : string | undefined = process.env[envVar]
-  if (variable === undefined) {
+  if (variable === undefined || variable === '') {
     throw new Error(`Required variable ${envVar} is not defined`)
   }
   return variable
@@ -18,11 +18,14 @@ const req = (envVar: string) : string => {
 
 const setDefault = (envVar: string, defValue: string) : string => process.env[envVar] || defValue
 
+const array = (envVar: string): string[] => req(envVar).split(',')
+
 export const PORT : string = setDefault('PORT', '8080')
 export const NODE_ENV : string = req('NODE_ENV')
 export const PG_CONNECTION_STRING : string = req('PG_CONNECTION_STRING')
 export const GOOGLE_SIGN_IN_CLIENT_ID : string = req('GOOGLE_SIGN_IN_CLIENT_ID')
 export const GOOGLE_EMAIL_DOMAIN : string = req('GOOGLE_EMAIL_DOMAIN')
+export const ADMINS: string[] = array('ADMINS')
 
 export const db = knex({
   client: 'pg',
