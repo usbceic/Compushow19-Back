c0e41eb64f5a3593d158ffc68d6a9e50850e0ce3
Andrés Ignacio Torres <andresitorresm@gmail.com>
2019-11-15 21:21:20 -0400

    return nominations with user information on them

--- src/nominations/models.ts
+++ src/nominations/models.ts
@@ -1,4 +1,6 @@
 import { db } from '../config'
+import { RegisteredUser } from '../users/objects'
+import { getUserByUserId } from '../users/models'
 
 const TABLE_NAME = 'nominations'
 
@@ -20,9 +22,42 @@ export interface NominationModel {
   extra?: string
 }
 
-export interface ExtendedNominationModel extends NominationModel {
-  mainNomineeName?: string
-  auxNomineeName?: string
+export interface ExtendedNominationModel {
+  id?: number
+  userId: number
+  categoryId: number
+  mainNominee?: RegisteredUser
+  auxNominee?: RegisteredUser
+  extra?: string
+}
+
+export async function addNomineeToNomination(nomination: NominationModel) : Promise<ExtendedNominationModel> {
+  var mainNominee, auxNominee
+  if (nomination.mainNominee !== undefined) {
+    mainNominee = await getUserByUserId(nomination.mainNominee) as RegisteredUser
+  }
+  if (nomination.auxNominee !== undefined) {
+    auxNominee = await getUserByUserId(nomination.auxNominee) as RegisteredUser
+  }
+
+  const extendedNomination : ExtendedNominationModel = {
+    id: nomination.id,
+    userId: nomination.userId,
+    categoryId: nomination.categoryId,
+    mainNominee: mainNominee,
+    auxNominee: auxNominee
+  }
+
+  return extendedNomination
+}
+
+export async function addNomineesToNominations(nominations: NominationModel[]) : Promise<ExtendedNominationModel[]> {
+  var extendedNominations : ExtendedNominationModel[] = []
+  nominations.forEach(async (n) => {
+    extendedNominations.push(await addNomineeToNomination(n))
+  })
+
+  return extendedNominations
 }
 
 export async function getNominationById(id: number) : Promise<NominationModel> {
