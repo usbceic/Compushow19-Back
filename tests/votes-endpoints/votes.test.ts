579480fb721e7ddae6880f8fb90c4fa8850b715e
German Robayo <germanrobayo33@gmail.com>
2019-11-20 17:38:23 -0400

    add tets for get nominee per category

--- tests/votes-endpoints/votes.test.ts
+++ tests/votes-endpoints/votes.test.ts
@@ -6,10 +6,12 @@ import { CreateCategoryRequest } from '../../src/categories/objects'
 import { CreateVoteRequest } from '../../src/votes/objects'
 import { Nominee } from '../../src/nominees/objects'
 import { VoteModel } from '../../src/votes/models'
+import { SavedCategoryModel } from '../../src/categories/models'
 jest.mock('google-auth-library')
 
 const ADMIN_TOKEN = 'admin_token'
 const NON_ADMIN_TOKEN = 'user_token'
+let category: SavedCategoryModel, nominees: Nominee[]
 
 beforeAll(async () => {
   OAuth2Client.prototype.verifyIdToken = async function(options: any): Promise<any> {
@@ -24,7 +26,31 @@ beforeAll(async () => {
     }
   }
   await db.migrate.latest()
-  return await db.seed.run()
+  await db.seed.run()
+  const expected : CreateCategoryRequest =   {
+    color: '#000',
+    description: 'Category for voting',
+    name: 'Category for voting',
+    pictureUrl: 'http://picture.url',
+    type: 'TO_USER'
+  }
+  const res = await request(app)
+    .post('/v1/api/categories')
+    .send(expected)
+    .set('Authorization', `Bearer ${ADMIN_TOKEN}`)
+  expect(res.status).toBe(201)
+
+  category = res.body
+
+  nominees = await Promise.all(['1', '2'].map(async i => {
+    const [row] = await db('nominees').insert({
+      categoryId: res.body.id!!,
+      happyPictureUrl: 'http://happy.com',
+      sadPictureUrl: 'http://sad.com',
+      name: `Nominee ${i}`
+    }).returning(['id', 'name'])
+    return row
+  }))
 })
 
 afterAll(async () => {
@@ -37,30 +63,6 @@ describe('Votes management', () => {
   describe('Votes creation', () => {
     const url = baseUrl
     it('let you successfully vote for a nominee in a category and to not repeat', async () => {
-      await db.migrate.latest()
-      await db.seed.run()
-      const expected : CreateCategoryRequest =   {
-        color: '#000',
-        description: 'Category for voting',
-        name: 'Category for voting',
-        pictureUrl: 'http://picture.url',
-        type: 'TO_USER'
-      }
-      const res = await request(app)
-        .post('/v1/api/categories')
-        .send(expected)
-        .set('Authorization', `Bearer ${ADMIN_TOKEN}`)
-      expect(res.status).toBe(201)
-
-      const nominees: Nominee[] = await Promise.all(['1', '2'].map(async i => {
-        const [row] = await db('nominees').insert({
-          categoryId: res.body.id!!,
-          happyPictureUrl: 'http://happy.com',
-          sadPictureUrl: 'http://sad.com',
-          name: `Nominee ${i}`
-        }).returning(['id', 'name'])
-        return row
-      }))
       const [nominee1, nominee2] = nominees
       const voteRequest: CreateVoteRequest = {
         nomineeId: nominee1.id!!
@@ -90,6 +92,52 @@ describe('Votes management', () => {
       expect(voteResponse2.status).toBe(409)
       const {length: newLength}: VoteModel[] = await db('votes').select('*')
       expect(newLength).toBe(length)
+
+      const voteResponseFromOtherUser = await request(app)
+        .post(url)
+        .send(voteRequest)
+        .set('Authorization', `Bearer ${ADMIN_TOKEN}`)
+      expect(voteResponseFromOtherUser.status).toBe(204)
+    })
+  })
+  describe('Votes retrieving', () => {
+    const url = `${baseUrl}/byCategory`
+    it('Returns 404 when the category doesn\'t have any vote from current user', async () => {
+      await db('votes').delete()
+      const voteResponse = await request(app)
+        .get(`${url}/${category.id}`)
+        .set('Authorization', `Bearer ${NON_ADMIN_TOKEN}`)
+      expect(voteResponse.status).toBe(404)
+    })
+    it('Returns 200 when the user has made a vote on that category', async () => {
+      let [nominee] = nominees
+      await db('votes').delete()
+      await request(app)
+        .post(`${baseUrl}`)
+        .send({
+          nomineeId: nominee.id!!
+        })
+        .set('Authorization', `Bearer ${NON_ADMIN_TOKEN}`)
+
+      const voteResponse = await request(app)
+        .get(`${url}/${category.id}`)
+        .set('Authorization', `Bearer ${NON_ADMIN_TOKEN}`)
+      expect(voteResponse.status).toBe(200)
+    })
+    it('the vote from user a is different from user b', async () => {
+      let [nominee] = nominees
+      await db('votes').delete()
+      await request(app)
+        .post(`${baseUrl}`)
+        .send({
+          nomineeId: nominee.id!!
+        })
+        .set('Authorization', `Bearer ${ADMIN_TOKEN}`)
+
+      const voteResponse = await request(app)
+        .get(`${url}/${category.id}`)
+        .set('Authorization', `Bearer ${NON_ADMIN_TOKEN}`)
+      expect(voteResponse.status).toBe(409)
     })
   })
 })
